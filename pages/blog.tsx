22e6a664b7deddf9b8b4ca92db24d4bdfece6f28
aminelfekih <elfekihamine@gmail.com>
2019-12-02 15:30:08 +0100

    add blog page

---
+++ pages/blog.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import Layout from '../components/Layout';
+
+const Blog: React.FC = () => (
+  <Layout>
+    <h1 data-testid="page-title">Blog</h1>
+  </Layout>
+);
+
+export default Blog;
